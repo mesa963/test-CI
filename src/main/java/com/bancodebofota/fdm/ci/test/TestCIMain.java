8be83817b83ba1c33d00305ac48dc35d5f1d4adc
Jamm <jmesa1@bancodebogota.com.co>
2018-07-31 18:20:15 -0500

    Commit Puntoseis

---
+++ src/main/java/com/bancodebofota/fdm/ci/test/TestCIMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bancodebofota.fdm.ci.test;
+
+/**
+ *
+ * @author Jamm
+ */
+public class TestCIMain {
+    public int sum(int n1,int n2)
+    {
+       return n1+ n1;
+    }
+
+    public static void  main (String [] args)
+    {
+      System.err.println("TESTCI");
+    }
+
+    
+
+}
